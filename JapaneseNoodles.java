d714263d38c9850cb300dd37426b369a510da0e3
ashlesh9999 <90056040+ashlesh9999@users.noreply.github.com>
2021-09-09 16:15:55 -0500

    Create JapaneseNoodles.java

---
+++ JapaneseNoodles.java
@@ -0,0 +1,6 @@
+public class JapaneseNoodles implements Noodles {
+    @Override
+    public void taste() {
+        System.out.println("Japanese noodles are salty and deep nutty flavour");
+    }
+}
